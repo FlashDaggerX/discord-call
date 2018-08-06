79f2b41499ef976ef981e4a8921c1e37ed90eb5e
BuildTools <unconfigured@null.spigotmc.org>
2018-08-06 15:12:21 -0700

    Update

---
+++ lua/autorun/init.lua
@@ -0,0 +1,3 @@
+include("config.lua")
+include("call.lua")
+print("Initialized Discord Calls!")
---
+++ lua/config.lua
@@ -0,0 +1,7 @@
+discordcall = {}
+
+//Enable or Disable the addon. (Boolean)
+discordcall.enabled = true
+
+//Create a discord webhook for the channel you want the messages in, then paste the link in here. This is REQUIRED! (String)
+discordcall.webhook = "NOT SET"
