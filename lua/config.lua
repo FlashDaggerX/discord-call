7835c7671391d495f67fbc1817fde19f3414fb82
BuildTools <unconfigured@null.spigotmc.org>
2018-08-07 23:59:55 -0700

    WHOOPS

--- lua/config.lua
+++ lua/config.lua
@@ -4,7 +4,7 @@ discordcall = {}
 discordcall.enabled = true
 
 //Create a discord webhook for the channel you want the messages in, then paste the link in here. This is REQUIRED! (String, default="NOT SET")
-discordcall.webhook = "https://canary.discordapp.com/api/webhooks/459857199745859584/nhpKKxm4LheBEJGL7LCY0VQhZmKYQStp1WxOzFOLEi1Oukrd4PmwMe-kpYS7Cva8EfF3"
+discordcall.webhook = "NOT SET"
 
 //Number (in minutes) between calls per person. (Number, default=5)
 discordcall.timeout = 5
