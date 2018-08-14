bb6abc90aa9123995c4fa16a1799196318eb3614
Kyle Guarco <kyleguarco55@gmail.com>
2018-08-14 14:06:09 -0400

    Cleaned up client call code

--- lua/autorun/init.lua
+++ lua/autorun/init.lua
@@ -1,12 +1,11 @@
 -- GLua Script
 
 if (SERVER) then
-    include("../config.lua")
-
     -- Pool "CallDiscord" before the serverside reciever is loaded.
     util.AddNetworkString("CallDiscord")
     include("../sv_call.lua")    
 else
     AddCSLuaFile("../cl_call.lua")
+    include("../cl_call.lua")
 end
 print("discord-calls Initialized!")
--- lua/cl_call.lua
+++ lua/cl_call.lua
@@ -10,23 +10,26 @@ include("win/create_window.lua")
 	net.SendToServer()
 end)]]--
 
-hook.Add("OnPlayerChat", "!calldiscord",function()
+hook.Add("OnPlayerChat", "!calldiscord", function(ply, text, teamChat, isDead)
 
-	win:CreateDFrame		(nil, "Help!", {}, {x=400, y=175})
-	win:CreateDLabel		(win:GetActiveWindow(), "Reason", {x=20, y=50}, {})
-	win:CreateDTextEntry	(win:GetActiveWindow(), "Entry", "", {x=80, y=50}, {x=300, y=20}, nil)
-	win:CreateDButton		(win:GetActiveWindow(), "Send!", {x=100, y=100}, {x=200, y=50}, 
-	
-	function(self)
-		local reason = win:GetElements("Entry"):GetText()
-		if (reason == "") then
-			LocalPlayer():ChatPrint("You need to enter a reason!")
-		else
-			net.Start("CallDiscord")
-			net.WriteString(reason)
-			net.SendToServer()
+	if text == "!calldiscord" then
+		win:CreateDFrame		(nil, "Help!", {}, {x=400, y=175})
+		win:CreateDLabel		(win:GetActiveWindow(), "Reason", {x=20, y=50}, {})
+		win:CreateDTextEntry	(win:GetActiveWindow(), "Entry", "", {x=80, y=50}, {x=300, y=20}, nil)
+		win:CreateDButton		(win:GetActiveWindow(), "Send!", {x=100, y=100}, {x=200, y=50}, 
+		
+		function(self)
+			local reason = win:GetElements("Entry"):GetText()
+			if (reason == "") then
+				LocalPlayer():ChatPrint("You need to enter a reason!")
+			else
+				net.Start("CallDiscord")
+				net.WriteString(reason)
+				net.SendToServer()
 
-			win:KillWindow()
-		end
-	end)
+				win:KillWindow()
+			end
+		end)
+		
+	end
 end)
