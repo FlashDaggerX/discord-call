c2abba9be6e1b5763d61fbb0e4dcc1ee4732e186
Kyle Guarco <kyleguarco55@gmail.com>
2018-08-14 18:04:26 -0400

    Removed enable check, just for a little while

--- lua/sv_call.lua
+++ lua/sv_call.lua
@@ -1,27 +1,27 @@
 -- GLua Script
 
+local http = require "socket.http"
+
 include("sv_config.lua")
 
 -- (os.time()-ply:GetPData("LastDiscordCall",0)) > (60 * discordcall.timeout)
-
 net.Receive("CallDiscord",function(l,ply)
-	if (discordcall.enabled) then
-		local args = {}
+	local args = {}
 
-		args["durl"] = discordcall.webhook
-		args["svname"] = GetHostName()
-		args["joinurl"] = "steam://connect/"..game.GetIPAddress()
-		args["sid64"] = "76561198188070674"--ply:SteamID64()
-		args["reason"] = net.ReadString()
+	args["durl"] = discordcall.webhook
+	args["svname"] = GetHostName()
+	args["joinurl"] = "steam://connect/"..game.GetIPAddress()
+	args["sid64"] = "76561198188070674"--ply:SteamID64()
+	args["reason"] = net.ReadString()
+	
+	PrintTable(args)
+	
+	http.Post(discordcall.webhook, args, function(res,ln,head,code)
+		print("Success!")
+		print("["..code.."]")
+		print(res)
 		
-		PrintTable(args)
-		
-		http.Post(discordcall.webhook, args, function(res,ln,head,code)
-			print("Success!")
-			print("["..code.."]")
-			print(res)
-			
-			ply:ChatPrint("Sent message successfully! Please wait patiently for a response.")
-		end)
-	end
+		ply:ChatPrint("Sent message successfully! Please wait patiently for a response.")
+	end)
+	
 end)
--- lua/sv_config.lua
+++ lua/sv_config.lua
@@ -3,7 +3,7 @@
 discordcall = {}
 
 -- Enable or Disable the addon. (Boolean, default=true)
-discordcall.enabled = true
+discordcall.enable = true
 
 -- Create a discord webhook for the channel you want the messages in, then paste the link in here. This is REQUIRED! (String, default="NOT SET")
 discordcall.webhook = "https://discordapp.com/api/webhooks/478937293961297920/7BsfM0jxImaMudOyHOycSpigK8Gve4TY5N8v2wXVqTl4jJp1QAT_V2xgFBmOrpqJ5D0n"
