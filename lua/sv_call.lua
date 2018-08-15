3ceb927d60d1003990d4b7f7826c085c747d2105
Kyle Guarco <kyleguarco55@gmail.com>
2018-08-15 09:05:48 -0400

    Changed 'content'

--- lua/sv_call.lua
+++ lua/sv_call.lua
@@ -11,20 +11,38 @@ include("autorun/config.lua")
 	]]
 
 net.Receive("CallDiscord",function(l,ply)
+	local pid = ply:SteamID64()
+	if pid == nil then pid = "<SingleplayerSTEAM64>" end
+
+	local post = 
+	{
+		content = "A message from STEAMID:"..pid,
+		embeds = 
+		{
+			{
+				title = GetHostName(),
+				url = "steam://connect/"..game.GetIPAddress()
+			}
+		}
+	}
+	
 	http.Post(
-		discordcall.webhook,
-		
-		{ 
-			content = "Done!" 
-		},
-		
+		discordcall.webhook, post, 
+
 		function(resp, length, headers, status)
 			print(resp)
+
+			ply:SetPData("LastDiscordCall", os.time())
+			ply:ChatPrint("Sent message successfully! Please wait patiently for a response.")
 		end,
 
 		function(err)
 			print(err)
 		end
 	)
-	
+
+	--[[if discordcall.enabled then
+		if (os.time() - ply:GetPData("LastDiscordCall", 0)) > (60 * discordcall.timeout) then
+		end
+	end]]
 end)
