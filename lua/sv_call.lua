c855c8f67f85c8fa4b5a11073d83e96a52f98a87
Kyle Guarco <kyleguarco55@gmail.com>
2018-08-14 20:29:56 -0400

    Added discord JSON parameter 'content'

--- lua/sv_call.lua
+++ lua/sv_call.lua
@@ -1,27 +1,30 @@
 -- GLua Script
 
-local http = require "socket.http"
-
-include("sv_config.lua")
+include("autorun/config.lua")
 
 -- (os.time()-ply:GetPData("LastDiscordCall",0)) > (60 * discordcall.timeout)
-net.Receive("CallDiscord",function(l,ply)
-	local args = {}
+--[[ args["durl"] = discordcall.webhook
+	 args["svname"] = GetHostName()
+	 args["joinurl"] = "steam://connect/"..game.GetIPAddress()
+	 args["sid64"] = "76561198188070674"--ply:SteamID64()
+	 args["reason"] = net.ReadString()
+	]]
 
-	args["durl"] = discordcall.webhook
-	args["svname"] = GetHostName()
-	args["joinurl"] = "steam://connect/"..game.GetIPAddress()
-	args["sid64"] = "76561198188070674"--ply:SteamID64()
-	args["reason"] = net.ReadString()
-	
-	PrintTable(args)
-	
-	http.Post(discordcall.webhook, args, function(res,ln,head,code)
-		print("Success!")
-		print("["..code.."]")
-		print(res)
+net.Receive("CallDiscord",function(l,ply)
+	http.Post(
+		discordcall.webhook,
 		
-		ply:ChatPrint("Sent message successfully! Please wait patiently for a response.")
-	end)
+		{ 
+			content = "Done!" 
+		},
+		
+		function(resp, length, headers, status)
+			print(resp)
+		end,
+
+		function(err)
+			print(err)
+		end
+	)
 	
 end)
